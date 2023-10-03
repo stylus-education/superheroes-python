ee8292126fe58991cd71aff7d34360af254f810a
Alex Marriott <alex.marriott@buildcircle.co.uk>
2023-10-03 11:21:00 +0100

    Fix type lacking quotes, change single quotes to double quotes

--- tests/test_superheroes_python.py
+++ tests/test_superheroes_python.py
@@ -7,12 +7,12 @@ def test_battle_returns_the_hero_if_they_have_a_higher_score(mocker):
         return_value=
         {
             "items": [
-                {"name": 'Winner', "score": 9.0, type: 'hero'},
-                {"name": 'Loser', "score": 8.0, type: 'villain'}
+                {"name": "Winner", "score": 9.0, "type": "hero"},
+                {"name": "Loser", "score": 8.0, "type": "villain"}
             ]
         }
     )
 
-    assert battle("Winner", "Loser") == {"name": 'Winner', "score": 9.0, type: 'hero'}
+    assert battle("Winner", "Loser") == {"name": "Winner", "score": 9.0, "type": "hero"}
 
 
